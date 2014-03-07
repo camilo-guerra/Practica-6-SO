b4f28e5ae16617c379cdf683ce05e9acb0219b5f
camilo-guerra <yamto.elsuperior@gmail.com>
2014-03-07 06:53:45 +0000

    Create Script.sh

---
+++ Script.sh
@@ -0,0 +1,24 @@
+
+echo $1 
+$2 
+$3
+
+
+alias FAT32View=view
+alias FAT32Ls=ls
+alias FAT32mkdir=mkdir
+
+
+alias FAT32CreateFile="./d.sh"
+
+STARTFGCOLOR='\e[0;34m';
+STARTBGCOLOR="\e[45m"
+ENDCOLOR="\e[0m"
+export PS1="$STARTFGCOLOR$STARTBGCOLOR\u@\h \w> $ENDCOLOR"
+
+
+
+
+ 
+PS1="FAT32Utility>"
+
